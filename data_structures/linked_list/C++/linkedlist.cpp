69baa5d5de9e1966183a89b1ed2117280775a874
ennaangel <chrisvos1992@gmail.com>
2023-09-23 23:37:23 +0200

    Added the add node method and some documentation

--- data_structures/linked_list/C++/linkedlist.cpp
+++ data_structures/linked_list/C++/linkedlist.cpp
@@ -1,4 +1,5 @@
 struct Node
+//A Linked list node
 {
     string value;
     Node* next;
@@ -11,13 +12,25 @@ class LinkedList{
             tail = nullptr;
             };
         void add_node(string value){
-            node* temp = new Node;
+            node* temp = new Node;      //Store the pointer to the new node in temp
+            temp->value = value;        //Assign the value to the value stored at temp
+            temp->next = nullptr;       //Add the next pointer to null
 
-        }
+            if (head == nullptr)        //If it is the first node, assign it as head and tail
+                {
+                    head = temp;
+                    tail = temp;
+                }
+            else                        //Else point the tail.next to temp, and assign temp only as tail,
+                {   
+                    tail->next = temp;
+                    tail = temp;
+                }
 
-
+            }
 
     private:
-        Node *head;
+        Node* head;
+        Node* tail;
     };
 

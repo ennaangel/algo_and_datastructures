cde121f6a3da926360b4d10a7c0e97a5e84e760f
ennaangel <chrisvos1992@gmail.com>
2023-09-23 23:16:04 +0200

    Initial code adding node and linkedList class

---
+++ data_structures/linked_list/C++/linkedlist.cpp
@@ -0,0 +1,23 @@
+struct Node
+{
+    string value;
+    Node* next;
+};
+
+class LinkedList{
+    public:
+        LinkedList(){
+            head = nullprt;
+            tail = nullptr;
+            };
+        void add_node(string value){
+            node* temp = new Node;
+
+        }
+
+
+
+    private:
+        Node *head;
+    };
+

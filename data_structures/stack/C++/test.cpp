ee7cf478865a5c8be46377e7da4175b28cff5dab
ennaangel <chrisvos1992@gmail.com>
2023-09-30 02:05:16 +0200

    Added counter for n tests failed

--- data_structures/stack/C++/test.cpp
+++ data_structures/stack/C++/test.cpp
@@ -3,18 +3,28 @@
 #include <iostream>
 
 class testStack{
+    //Class to test the stack datastructure
     private:
         PrintLogger logger;
+        int numberTestsFailed;
     public:
+    testStack(){
+        numberTestsFailed = 0;
+    }
     int testFirstPush(stack counts){
         counts.push("First");
+        int result = 0;
         if (counts.top-> name != "First"){
             logger.log("[Error] first push failed: Top is not equal to First");
-            return 1;
+            result += 1;
         }
         if (counts.bottum-> name != "First"){
             logger.log("[Error] first push failed: bottum is not equal to First");
-            return 1;
+            result += 1;
+        }
+        if (result > 0){
+            numberTestsFailed += result;
+            return result;
         }
         logger.log("[Info] First push passed: Top and bottum are both equal to First");
         return 0;
@@ -22,17 +32,29 @@ class testStack{
     int testSecondPush(stack counts){
         counts.push("First");
         counts.push("Second");
+        int result = 0;
         if (counts.top-> name != "Second"){
             logger.log("[Error] Second push failed: Top is not equal to Second");
-            return 1;
+            result += 1;
         }
         if (counts.bottum-> name != "First"){
             logger.log("[Error] second push failed: bottum is not equal to First");
-            return 1;
+            result += 1;
+        }
+        if (result > 0){
+            numberTestsFailed += result;
+            return result;
         }
         logger.log("[Info] Second push passed: Top equal to Second and bottum equal to First");
         return 0;
     };
+
+    int getNumberTestsFailed(){
+        string resultMessage = "[INFO] Number of tests failed: ";
+        resultMessage += std::to_string(numberTestsFailed);
+        logger.log(resultMessage);
+        return numberTestsFailed;
+    }
 
 };
 
@@ -42,4 +64,5 @@ void main(){
     testStack stacktester;
     stacktester.testFirstPush(counts);
     stacktester.testSecondPush(counts);
+    stacktester.getNumberTestsFailed();
 }
